1cef22fb217efd90887a869000873ced394c97ef
P3tarK01ev <pkole@softserveinc.com>
2018-05-14 17:11:55 +0300

    webpack for react

---
+++ webpack-for-react/dist/static/NoMatch.491d51792074226197aa.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{197:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var u=o(t(0)),l=t(61),a=o(t(79));function o(e){return e&&e.__esModule?e:{default:e}}n.default=function(){return u.default.createElement(a.default,null,u.default.createElement(l.Icon,{name:"minus circle",size:"big"}),u.default.createElement("strong",null,"Page not found!"))}}}]);
+//# sourceMappingURL=NoMatch.491d51792074226197aa.js.map
