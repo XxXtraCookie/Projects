6a3c4b295705f8eb5c0e5a792de2282107b836e7
P3tarK01ev <pkole@softserveinc.com>
2018-05-15 11:46:09 +0300

    webpack for react

---
+++ webpack-for-react/src/index.js
@@ -0,0 +1,20 @@
+import { AppContainer } from 'react-hot-loader';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import '../src/main.scss'; // FOR SASS
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+const render = Component =>
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) module.hot.accept('./components/App', () => render(App));
