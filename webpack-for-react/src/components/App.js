d43efce0abaa8145a31ca90a2bbc9bea00431d8d
Petar Kolev <33326233+MyTrainingAccount@users.noreply.github.com>
2018-05-17 00:23:08 +0300

    wip

--- webpack-for-react/src/components/App.js
+++ webpack-for-react/src/components/App.js
@@ -1,34 +1,30 @@
 import React from 'react';
-import {
-  Switch,
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+// import {
+//   Switch,
+//   BrowserRouter as Router,
+//   Route
+// } from 'react-router-dom';
 import importedComponent from 'react-imported-component';
 
 import Header from '../components/Header';
+import MainSection from '../components/MainSection';
 import Explore from '../components/Explore';
 
-// const AsyncDynamicPage = importedComponent(
-//   () => import('./DynamicPage'), {LoadingComponent: Loading}
-// );
-
-// const AsyncNoMatch = importedComponent(
-//   () => import('./NoMatch'), {LoadingComponent: Loading}
-// );
+    //   <Router>
+    //     <div>
+    //       <Switch>
+    //         <Route exact path="/" component={Explore} />
+    //       </Switch>
+    //     </div>
+    //   </Router>
 
 const App = () => {
   return (
     <div className="main-container">
     <Header />
-
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Explore} />
-          </Switch>
-        </div>
-      </Router>
+    <MainSection>
+      <Explore />
+    </MainSection>
     </div>
   );
 };
---
+++ webpack-for-react/src/components/MainSection/MainSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MainSection extends React.Component {
+  render() {
+    return (
+      // TODO: look how children work in REACT !!!
+      {...children}
+    );
+  }
+}
+
+export default MainSection;
