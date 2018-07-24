57abe3562c4ae7f046247cfd1f54daa24887bb36
peshonator <petar_g_kolev@abv.bg>
2018-07-24 16:31:08 +0300

    wip: subscribing to state

--- Advanced React.js/advanced-react/lib/components/App.js
+++ Advanced React.js/advanced-react/lib/components/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import pickBy from 'lodash.pickby';
+
 import ArticleList from './ArticleList';
+import SearchBar from './SearchBar';
+import Timestamp from './Timestamp';
 
 class App extends React.Component {
   static childContextTypes = {
     store: PropTypes.object,
   };
+
+  state = this.props.store.getState();
+
+  componentDidMount() {
+    this.subscriptionId = this.props.store.subscribe(this.onStoreChange);
+    this.props.store.startClock();
+  }
+
+  UNSAFE_componentWillUnmount() {
+    this.props.store.unsubscribe(this.subscriptionId);
+  }
+
+  onStoreChange = () => {
+    this.setState(this.props.store.getState());
+  }
 
   getChildContext() {
     return {
@@ -13,14 +32,22 @@ class App extends React.Component {
     };
   }
 
-  state = this.props.store.getState();
-
   render() {
+    let { articles, searchTerm } = this.state;
+    const searchRE = new RegExp(searchTerm, 'i');
+    if (searchTerm) {
+      articles = pickBy(articles, (value) => {
+        return value.title.match(searchRE) ||
+          value.body.match(searchRE);
+      });
+    }
+
     return (
-      <ArticleList
-        articles={this.state.articles}
-        store={this.props.store}
-      />
+      <div>
+        <Timestamp />
+        <SearchBar />
+        <ArticleList articles={articles} />
+      </div>
     );
   }
 }
--- Advanced React.js/advanced-react/lib/state-api/lib/index.js
+++ Advanced React.js/advanced-react/lib/state-api/lib/index.js
@@ -3,7 +3,12 @@ class StateApi {
     this.data = {
       articles: this.mapIntoObject(rawData.articles),
       authors: this.mapIntoObject(rawData.authors),
+      searchTerm: '',
+      timestamp: new Date(),
     };
+
+    this.subscriptions = {};
+    this.lastSubscriptionId = 0;
   }
 
   mapIntoObject(arr) {
@@ -16,6 +21,42 @@ class StateApi {
   lookupAuthor = (authorId) => this.data.authors[authorId];
 
   getState = () => this.data;
+
+  subscribe = (cb) => {
+    this.lastSubscriptionId++;
+    this.subscriptions[this.lastSubscriptionId] = cb;
+    return this.lastSubscriptionId;
+  }
+
+  unsubscribe = (subscriptionId) => {
+    delete this.subscriptions[subscriptionId];
+  }
+
+  notifySubscribers = () => {
+    Object.values(this.subscriptions).forEach((cb) => cb());
+  }
+
+  mergeWithState = (stateChange) => {
+    this.data = {
+      ...this.data,
+      ...stateChange
+    };
+    this.notifySubscribers();
+  }
+
+  setSearchTerm = (searchTerm) => {
+    this.mergeWithState({
+      searchTerm,
+    });
+  }
+
+  startClock = () => {
+    setInterval(() => {
+      this.mergeWithState({
+        timestamp: new Date(),
+      });
+    }, 1000);
+  }
 
 }
 
