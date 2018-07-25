2cf1bf782a60b2b10bd33f871ffd8f16f71a735e
peshonator <petar_g_kolev@abv.bg>
2018-07-25 17:01:08 +0300

    wip: initial app structure

---
+++ React and Redux in ES6/react-redux-app/src/components/Course/CoursesPage.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+
+class CoutrsesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+CoutrsesPage.propTypes = {};
+
+export default CoutrsesPage;
---
+++ React and Redux in ES6/react-redux-app/src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/App';
+import HomePage from './components/Home/HomePage';
+import AboutPage from './components/About/AboutPage';
+import CoutrsesPage from './components/Course/CoursesPage';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage} />
+    <Route path="courses" component={CoutrsesPage} />
+    <Route path="about" component={AboutPage} />
+  </Route>
+);
