8ff799bcd4508fd40b6407f4bd283c26a00d043d
peshonator <petar_g_kolev@abv.bg>
2018-07-27 16:47:43 +0300

    wip: testing redux

--- React and Redux in ES6/react-redux-app/src/components/Course/__tests__/CourseForm.Enzyme.test.js
+++ React and Redux in ES6/react-redux-app/src/components/Course/__tests__/CourseForm.Enzyme.test.js
@@ -1,7 +1,7 @@
 import expect from 'expect';
 import React from 'react';
 import { shallow } from 'enzyme';
-import CourseForm from './CourseForm';
+import CourseForm from '../CourseForm';
 
 function setup(saving) {
   const props = {
