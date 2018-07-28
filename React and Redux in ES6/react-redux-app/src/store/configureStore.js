f407698809415191557720e72c556eaa265a2e29
peshonator <petar_g_kolev@abv.bg>
2018-07-28 16:22:29 +0300

    wip: added redux dev tools

--- React and Redux in ES6/react-redux-app/src/store/configureStore.js
+++ React and Redux in ES6/react-redux-app/src/store/configureStore.js
@@ -1,15 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import pluralsightAdministration from '../reducers/pluralsightAdministration';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+
+const composeEnhancers = typeof window !== 'undefined' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 function configureStore(initialState) {
   return createStore(
     pluralsightAdministration,
     initialState,
-    applyMiddleware(
-      thunk,
-      reduxImmutableStateInvariant()
+    composeEnhancers(
+      applyMiddleware(
+        thunk,
+        reduxImmutableStateInvariant()
+      )
     )
   );
 }
