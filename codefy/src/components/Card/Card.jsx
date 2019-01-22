3a4f72e9c0d8b060c42c32e6aa0dccd732b9436d
XxXtraCookie <petar_g_kolev@abv.bg>
2019-01-22 19:33:28 +0200

    adding new cards

--- codefy/src/components/Card/Card.jsx
+++ codefy/src/components/Card/Card.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './card.scss';
 
-const htmlImg = require('../../_images/html_5.png');
-
 class Card extends React.Component {
   render() {
     return (
       <div className="card-wrapper">
-        <div className="image-wrapper">
-          <img src={htmlImg} ALT="HTML" />
+        <div className="card-container">
+          <div className="image-wrapper">
+            <img src={this.props.imgSrc} alt={this.props.imgAlt} />
+          </div>
+          <span className="title">{this.props.description}</span>
         </div>
-        <span className="title">Hyper Text Markup Language</span>
       </div>
     );
   }
--- codefy/src/components/MainBody/MainBody.jsx
+++ codefy/src/components/MainBody/MainBody.jsx
@@ -3,11 +3,45 @@ import './mainBody.scss';
 
 import Card from '../Card';
 
+const htmlImg = require('../../_images/html_5_v2.png');
+const cssImg = require('../../_images/css_3.png');
+const jsImg = require('../../_images/javascript.png');
+const reactImg = require('../../_images/react.svg');
+const reduxImg = require('../../_images/redux_v2.svg');
+
 class MainBody extends React.Component {
   render() {
     return (
       <div className="main-body-wrapper">
-        <Card />
+        <Card
+          imgSrc={htmlImg}
+          imgAlt="HTML"
+          description="Hyper Text Markup Language"
+        />
+
+        <Card
+          imgSrc={cssImg}
+          imgAlt="CSS"
+          description="Cascading Style Sheets"
+        />
+
+        <Card
+          imgSrc={jsImg}
+          imgAlt="JavaScript"
+          description="JavaScript"
+        />
+
+        <Card
+          imgSrc={reactImg}
+          imgAlt="ReactJS"
+          description="ReactJS"
+        />
+
+        <Card
+          imgSrc={reduxImg}
+          imgAlt="Redux"
+          description="Redux"
+        />
       </div>
     );
   }
